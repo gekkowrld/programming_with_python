21138ef2db4e539c1bb9740165f9a2f5b7cf8495
Gekko Wrld <gekkowrld@gmail.com>
2024-02-26 20:36:54 +0300

    Squashed commit of the following:
       * Restyled by yapf
    
       *Restyled by reorder-python-imports
    
       * Restyled by pyment
    
        *Restyled by isort
    
        *Restyled by black
    
        *Restyled by autopep8
    
        *chore: Add `circle`, `rectangle` and `temp`
    
        Add classes to calculate:
    
        - circle.py
        	Area and circumference of a circle
    
        - rectangle.py
        	Area and perimeter of a rectangle
    
        - temp_fan_cel.py
        	Convert between Celcius and Farenheit
    
    Signed-off-by: Gekko Wrld <gekkowrld@gmail.com>

---
+++ rectangle.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+"""
+A program to compute:
+    - Area of a rectangle
+    - Perimeter of a rectangle
+
+The arguments to be passed:
+    - lenght (int)
+    - width (int)
+
+"""
+
+
+class Rectangle:
+    """ """
+
+    def __init__(self, lenght, width):
+        self.lenght = lenght
+        self.width = width
+
+    def area(self):
+        """ """
+        return self.lenght * self.width
+
+    def perimeter(self):
+        """ """
+        return 2 * (self.lenght + self.width)
+
+
+clen = int(input("Enter len: "))
+win = int(input("Enter width: "))
+
+rect = Rectangle(clen, win)
+print("Area is: \n", rect.area())
+print("Perimeter is: \n", rect.perimeter())
